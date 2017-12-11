f7d72df6e8d888491cc73d577d2d48b5f1fa74ad
Ola Vea <ola@olavea.com>
2017-12-11 13:27:34 +0100

    Nextui.js-data.js

---
+++ Nextui.js-data.js
@@ -0,0 +1,85 @@
+var topologyData = {
+    nodes: [{
+        "id": 0,
+        "x": 410,
+        "y": 100,
+        "name": "HMS Dictator"
+    }, {
+        "id": 1,
+        "x": 410,
+        "y": 280,
+        "name": "HMS Surprise"
+    }, {
+        "id": 2,
+        "x": 660,
+        "y": 280,
+        "name": "SS Svanen"
+    }, {
+        "id": 3,
+        "x": 660,
+        "y": 100,
+        "name": "SS Vega"
+    }, {
+        "id": 4,
+        "x": 180,
+        "y": 190,
+        "name": "TS Concordia"
+    }],
+    links: [{
+        "source": 0,
+        "target": 1,
+        id: 0
+    }, {
+        "source": 0,
+        "target": 1,
+        id: 1
+    }, {
+        "source": 1,
+        "target": 0,
+        id: 2
+    }, {
+        "source": 1,
+        "target": 2,
+        id: 3
+    }, {
+        "source": 1,
+        "target": 3,
+        id: 4
+    }, {
+        "source": 4,
+        "target": 1,
+        id: 5
+    }, {
+        "source": 4,
+        "target": 1,
+        id: 6
+    }, {
+        "source": 2,
+        "target": 3,
+        id: 7
+    }, {
+        "source": 2,
+        "target": 3,
+        id: 8
+    }, {
+        "source": 2,
+        "target": 0,
+        id: 9
+    }, {
+        "source": 0,
+        "target": 4,
+        id: 10
+    }, {
+        "source": 0,
+        "target": 4,
+        id: 11
+    }, {
+        "source": 0,
+        "target": 3,
+        id: 12
+    }, {
+        "source": 0,
+        "target": 1,
+        id: 13
+    }, ]
+};
